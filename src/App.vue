70e8aa0a75520502b716f0c1b16e1b7dc5a4243f
1364544576@qq.com <qw19960529>
2020-11-05 18:26:34 +0800

    底部区域

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="wrap">
     <the-header></the-header>
+    <TheFooter/>
   </div>
 </template>
 
 <script>
   import TheHeader from '@/components/layouts/TheHeader';
+  import TheFooter from '@/components/layouts/TheFooter';
 
   export default {
     name: 'App',
     components:{
-      TheHeader
+      TheHeader,
+      TheFooter
     }
   }
 </script>
